c331198f956794e01c097275d3918d0701b7fb90
DavidTinoco <daviid.tc@gmail.com>
2017-03-01 12:30:03 +0100

    Modificación de enunciado del ejerecicio 2 (no regularidad en los datos) y ejercicio 3 completado

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Extracting data from a json file using python
 
 1. Lista todas las farmacias que tengan fax
-2. Contar cuantas farmacias hay abiertas en la hora que le pidamos
+2. Contar cuantas farmacias tienen un horario especial en verano
 3. Introducir la calle a buscar y nos dirá, si hay farmacia en esa calle, en qué número está la farmacia
 4. Introducir el nombre de la farmacia y nos dirá su horario.
 5. Introducir el nombre de la farmacia y nos saldrá una url de openstreetmap donde estará dicha farmacia.
--- farmacias.py
+++ farmacias.py
@@ -3,8 +3,30 @@ import json
 with open ("farmacias.json") as data_file:
     data = json.load(data_file)
 
+#1
+
 print "Las farmacias con fax son:"
     
 for j in data["directorios"]["directorio"]:
     if len(j["fax"]) > 5:
         print j["nombre"]["content"]
+
+#2 
+
+
+#3
+
+calle = raw_input("Introduzca la calle a buscar (C/,Avda.,Plaza): ")
+existe = False 
+for j in data["directorios"]["directorio"]:
+    if j["direccion"][0].startswith(calle):
+        existe = True
+        numero = j["direccion"][0]
+        print "En dicha calle se encuentra una farmacia en el número" + str(numero).split(",")[1]
+if not existe:
+    print "No hay ninguna farmacia en dich calle"        
+
+
+#4
+
+farmacia = raw_input("Introduzca el nombre de la farmacia: ")
