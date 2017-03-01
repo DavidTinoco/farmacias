2aca61e498da02e87388751cf72352e97175441d
DavidTinoco <daviid.tc@gmail.com>
2017-03-01 11:07:58 +0100

    Primer ejercicio acabado

--- farmacias.py
+++ farmacias.py
@@ -3,5 +3,8 @@ import json
 with open ("farmacias.json") as data_file:
     data = json.load(data_file)
 
+print "Las farmacias con fax son:"
+    
 for j in data["directorios"]["directorio"]:
-    print j
+    if len(j["fax"]) > 5:
+        print j["nombre"]["content"]
